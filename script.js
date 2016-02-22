aad4f9f9e5acd6ece024bc08bb70507ba3d74d66
dave1532 <davidrutkoske@gmail.com>
2016-02-22 09:29:35 -0800

    add js coler  back$white

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+// Your JS goes her
+
+var body = document.getElementsByTagName('body')[0];
+var tiles = []
+for (var i = 0; i < 63; i++){
+  tiles[i] =document.createElement('div');
+  tiles[i].style.width='11.1%';
+  tiles[i].style.float='left';
+  tiles[i].style.paddingBottom = '11.1%';
+  if ((i % 2)===0) {
+    tiles[i].style.backgroundColor = 'black';
+  }else{
+    tiles[i].style.backgroundColor = 'red';
+  }
+   body.appendChild(tiles[i]);
+  }
